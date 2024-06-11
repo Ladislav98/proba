4eab4341c43b38b2bb10b14adc263ace8ba10ee6
Ladislav <ladislav.zerjavic@gmail.com>
2024-06-11 23:33:30 +0200

    dispatch username

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate("/menu");
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <p className="mb-4 text-sm text-stone-600 md:text-base">
         Welcome 👋! Please start by telling us your name:
       </p>
@@ -19,7 +32,9 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button to="/menu">Start ordering</Button>
+          <Button type="primary" to="/menu">
+            Start ordering
+          </Button>
         </div>
       )}
     </form>
