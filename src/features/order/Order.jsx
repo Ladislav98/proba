bfc54cf4222c0ce337c7548f506233405322645a
Ladislav <ladislav.zerjavic@gmail.com>
2024-06-16 11:40:19 +0200

    order item, order priority, styling

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -6,6 +6,7 @@ import {
 } from "../../utils/helpers";
 import { useOrder } from "./useOrder";
 import Loader from "../../ui/Loader";
+import OrderItem from "./OrderItem";
 
 function Order() {
   const { orderId } = useParams();
@@ -38,29 +39,45 @@ function Order() {
     return <p>Order not found</p>;
   }
   return (
-    <div>
-      <div>
-        <h2>Status</h2>
+    <div className="px-4 py-6 space-y-6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <h2 className="text-xl font-semibold">Order #{id} status</h2>
 
-        <div>
-          {priority && <span>Priority</span>}
-          <span> {status} order</span>
+        <div className="space-x-2">
+          {priority && (
+            <span className="px-3 py-1 font-semibold tracking-wide uppercase bg-red-500 rounded-full text-red-50">
+              Priority
+            </span>
+          )}
+          <span className="px-3 py-1 font-semibold tracking-wide uppercase bg-green-500 rounded-full text-green-50">
+            {status} order
+          </span>
         </div>
       </div>
 
-      <div>
-        <p>
+      <div className="flex flex-wrap items-center justify-between gap-2 px-6 py-5 bg-stone-200">
+        <p className="font-medium">
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : "Order should have arrived"}
         </p>
-        <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+        <p className="text-xs text-stone-500">
+          (Estimated delivery: {formatDate(estimatedDelivery)})
+        </p>
       </div>
 
-      <div>
+      <ul className="border-t border-b divide-y divide-stone-200">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.name} />
+        ))}
+      </ul>
+
+      <div className="flex flex-col gap-2 px-6 py-5 bg-stone-200">
         <p>Price product: {formatCurrency(orderPrice)}</p>
         {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+        <p className="font-medium">
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   );
